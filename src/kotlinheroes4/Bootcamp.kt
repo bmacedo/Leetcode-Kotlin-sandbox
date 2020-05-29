ebdc8f50c95c1c0d54a3bc99ad29d73626e57645
Bernardo Macedo <bernardo-macedo@hotmail.com>
2020-05-29 20:31:21 +0200

    Kotlin Heroes - May 2020

---
+++ src/kotlinheroes4/Bootcamp.kt
@@ -0,0 +1,26 @@
+package kotlinheroes4
+
+fun main(args: Array<String>) {
+    val times = readLine()!!.toInt()
+    repeat(times) {
+        val (_, k1, k2) = readLine()!!.split(" ").mapNotNull { it.toIntOrNull() }
+        val str = readLine()!!
+        println(Bootcamp.maxLectures(str, k1, k2))
+    }
+}
+
+object Bootcamp {
+    fun maxLectures(schedule: String, maxPerDay: Int, maxConsecutiveDays: Int): Int {
+        val lectures = mutableListOf<Int>()
+        schedule.forEach {
+            if (it == '0') {
+                lectures.add(0)
+            } else {
+                val previousSum = lectures.lastOrNull() ?: 0
+                val allowedLectures = Math.min(maxPerDay, maxConsecutiveDays - previousSum)
+                lectures.add(allowedLectures)
+            }
+        }
+        return lectures.sum()
+    }
+}
