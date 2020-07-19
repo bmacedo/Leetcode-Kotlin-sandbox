0471e1d0497afbec6209a431ca908b3297034dc5
Bernardo Macedo <bernardo-macedo@hotmail.com>
2020-07-19 19:09:13 +0200

    Easy Collection - Reverse Linked List

---
+++ src/adhoc/ReverseLinkedList.kt
@@ -0,0 +1,24 @@
+package adhoc
+
+import utils.ListNode
+import utils.toList
+import utils.toListNode
+
+fun main(args: Array<String>) {
+    val input = intArrayOf(1, 2, 3, 4, 5).toListNode()
+    println(ReverseLinkedList.reverseList(input).toList())
+}
+
+object ReverseLinkedList {
+    fun reverseList(head: ListNode?): ListNode? {
+        var node = head
+        var prev: ListNode? = null
+        while (node != null) {
+            val temp = node.next
+            node.next = prev
+            prev = node
+            node = temp
+        }
+        return prev
+    }
+}
