ba620f7fe23870e5961b7f7dd713ef4c6a641aa4
Bernardo Macedo <bernardo-macedo@hotmail.com>
2020-08-03 21:50:43 +0200

    Day 3

---
+++ src/challenges/august30DaysChallenge/Leetcode30DayChallenge3.kt
@@ -0,0 +1,24 @@
+package challenges.august30DaysChallenge
+
+
+/**
+ * Valid Palindrome
+ *
+ * Given a string, determine if it is a palindrome, considering only alphanumeric characters
+ * and ignoring cases.
+ *
+ * Note: For the purpose of this problem, we define empty string as valid palindrome.
+ */
+fun main(args: Array<String>) {
+    val input = "race e car"
+    println(Leetcode30DayChallenge3.isPalindrome(input))
+}
+
+object Leetcode30DayChallenge3 {
+
+    fun isPalindrome(s: String): Boolean {
+        val originalStr = s.filter { it.isLetterOrDigit() }.toLowerCase()
+        val reversedStr = originalStr.reversed()
+        return originalStr == reversedStr
+    }
+}
