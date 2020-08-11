ee0118103fd2565484a106bce60fd49fe6da7d11
Bernardo Macedo <bernardo-macedo@hotmail.com>
2020-08-11 21:34:33 +0200

    Day 11

--- src/challenges/august30DaysChallenge/Leetcode30DayChallenge11.kt
+++ src/challenges/august30DaysChallenge/Leetcode30DayChallenge11.kt
@@ -1,5 +1,4 @@
-package challenges.june30DaysChallenge
-
+package challenges.august30DaysChallenge
 
 /**
  * H-Index
@@ -11,14 +10,24 @@ package challenges.june30DaysChallenge
  * have at least h citations each, and the other N − h papers have no more than h citations each."
  */
 fun main(args: Array<String>) {
-    val input = intArrayOf(0, 1, 3, 5, 6)
-    println(Leetcode30DayChallenge18.hIndex(input))
+    val citations = intArrayOf(3, 0, 6, 1, 5)
+    println(Leetcode30DayChallenge11.hIndex(citations))
 }
 
-object Leetcode30DayChallenge18 {
+object Leetcode30DayChallenge11 {
 
-    // O(n)
+    // O(n log n) time, O(1) space
     fun hIndex(citations: IntArray): Int {
+        val n = citations.size
+        citations.sortDescending()
+        for (i in 0 until n) {
+            if (i >= citations[i]) return i
+        }
+        return n
+    }
+
+    // O(n) time, O(n) space
+    fun hIndexLinear(citations: IntArray): Int {
         val count = IntArray(citations.size + 1)
         for (c in citations) {
             if (c <= citations.size) {
@@ -38,6 +47,5 @@ object Leetcode30DayChallenge18 {
         }
         return max
     }
-
 
 }
