6284f399982d9fdd63bbbb1c30881d53824cf6c2
Bernardo Macedo <bernardo-macedo@hotmail.com>
2020-10-06 00:05:13 +0200

    Day 5

---
+++ src/challenges/october30DaysChallenge/Day5.kt
@@ -0,0 +1,25 @@
+package challenges.october30DaysChallenge
+
+
+/**
+ * Complement of Base 10 Integer
+ *
+ * Every non-negative integer N has a binary representation.
+ * For example, 5 can be represented as "101" in binary, 11 as "1011" in binary, and so on.
+ * Note that except for N = 0, there are no leading zeroes in any binary representation.
+ *
+ * The complement of a binary representation is the number in binary you get when changing
+ * every 1 to a 0 and 0 to a 1.  For example, the complement of "101" in binary is "010" in binary.
+ *
+ * For a given number N in base-10, return the complement of it's binary representation as a base-10 integer.
+ */
+fun main(args: Array<String>) {
+    println(Day5.bitwiseComplement(5))
+}
+
+object Day5 {
+
+    fun bitwiseComplement(N: Int): Int {
+        return if (N == 0) 1 else N xor (Integer.highestOneBit(N) shl 1) - 1
+    }
+}
